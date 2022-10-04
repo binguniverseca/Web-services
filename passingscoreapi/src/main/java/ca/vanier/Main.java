62f0108720540ea5db838cf4dca3af40aa6be1c5
yukiyuli <yukiyuli6688@gmail.com>
2022-10-04 13:47:50 -0400

    add code in Main.java

--- passingscoreapi/src/main/java/ca/vanier/Main.java
+++ passingscoreapi/src/main/java/ca/vanier/Main.java
@@ -4,6 +4,7 @@ public class Main
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String value =args[0];
+        System.out.println(Scores.result(value));
     }
 }
